346a89e39a4cc56fc198f02057653e85cd5855d1
Suvradeep Dutta <79043992+Suvradeep-Dutta@users.noreply.github.com>
2025-07-13 23:52:12 +0530

    Update attendance_summary.py

--- Python/attendance_summary.py
+++ Python/attendance_summary.py
@@ -14,23 +14,27 @@ df_clean["Month"]= df_clean["Date"].dt.strftime("%b")
 # Step 4: Total number of days attended per employee
 attendance_count = df_clean[df_clean["Status"] == "Present"].groupby("Employee")["Date"].count().reset_index(name= "Days Present")
 
-# Step 5: Employees with full attendance
+# Step 5: Total number days each employee attended in each month
+monthly_attendance = df_clean.groupby(["Employee", "Month"]).size().reset_index(name= "Days Present")
+
+# Step 6: Employees with full attendance
 total_days = df_clean["Date"].nunique()
 full_attendance = attendance_count[attendance_count["Days Present"] == total_days]
 
-# Step 6: Days absent per employee
+# Step 7: Days absent per employee
 absences = df_clean[df_clean["Status"] == "Absent"].groupby("Employee")["Date"].count().reset_index(name= "Days Absent")
 
-# Step 7: Employees with the most absences
+# Step 8: Employees with the most absences
 top_absentees = absences.sort_values(by= "Days Absent", ascending= False).head(3)
 
-# Step 7: Consolidating data into separate sheets within a single Excel workbook
+# Step 9: Consolidating data into separate sheets within a single Excel workbook
 with pd. ExcelWriter("attendance_report.xlsx", engine= 'xlsxwriter') as writer:
     df_clean.to_excel(writer, sheet_name= "Cleaned Data", index= False)
     attendance_count.to_excel(writer, sheet_name= "Attendance Count", index= False)
+    monthly_attendance.to_excel(writer, sheet_name= "Monthly Tend", index= False)
     full_attendance.to_excel(writer, sheet_name= "Full Attendance", index= False)
     absences.to_excel(writer, sheet_name= "Days Absent", index= False)
     top_absentees.to_excel(writer, sheet_name= "Top Absentees", index= False)
 
-# Step 8: Confirming successful report generation
+# Step 10: Confirming successful report generation
 print("Attendance Report generated successfully!")
